bbfbfc95aac88fde3378205e9c0e94ce5cbcd584
fred <fred@q4os-desktop>
2024-10-04 22:27:22 +0200

    Ident

--- level_1/1_DisplayText_InstrSeq/stack_cynlinders.cpp
+++ level_1/1_DisplayText_InstrSeq/stack_cynlinders.cpp
@@ -3,19 +3,19 @@ nclude <iostream>
 using namespace std;
 int main()
 {
-deplacer(1, 2);
-deplacer(1, 3);
-deplacer(2, 3);
-deplacer(1, 2);
-deplacer(3, 1);
-deplacer(3, 2);
-		     deplacer(1, 2);
-		        deplacer(1, 3);
-			   deplacer(2, 3);
-			      deplacer(2, 1);
-			         deplacer(3, 1);
-				    deplacer(2, 3);
-				       deplacer(1, 2);
-				          deplacer(1, 3);
-					     deplacer(2, 3);
+  deplacer(1, 2);
+  deplacer(1, 3);
+  deplacer(2, 3);
+  deplacer(1, 2);
+  deplacer(3, 1);
+  deplacer(3, 2);
+  deplacer(1, 2);
+  deplacer(1, 3);
+  deplacer(2, 3);
+  deplacer(2, 1);
+  deplacer(3, 1);
+  deplacer(2, 3);
+  deplacer(1, 2);
+  deplacer(1, 3);
+  deplacer(2, 3);
 }
